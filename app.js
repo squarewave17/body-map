0f4e0e97ed1800a44a5bac7047e4df7d65749cec
paul <paul@squarewavedigital.co.uk>
2021-09-10 03:11:31 +0100

    model-working

--- app.js
+++ app.js
@@ -1,79 +1,92 @@
-const imgs = document.getElementById("imgs");
-const leftBtn = document.getElementById("left");
-const rightBtn = document.getElementById("right");
+const imgs = document.getElementById('imgs');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
 
-const img = document.getElementById("currentImg");
-const list = document.getElementById("list");
+const img = document.getElementById('currentImg');
+const list = document.getElementById('list');
+let dataSet;
+let newDataSet;
+fetch('data.json')
+  .then((response) => response.json())
+  .then((data) => (dataSet = data))
+  .then(() => console.log(dataSet))
+  .then(
+    () =>
+      (newDataSet = dataSet.filter(function (item) {
+        return item.ACF.body_area != null;
+      }))
+  )
+  .then(() => console.log(newDataSet));
 
-const testArray = [
+const controlArray = [
   {
-    img: "/img/zone1m.png",
-    ent: ["Aids", "Herpes", "Scurvy"],
-    lnk: ["#", "#", "#"],
+    img: '/img/zone1m.png',
+    zone: 'zone1',
   },
   {
-    img: "/img/zone2m.png",
-    ent: ["Shit Block", "Hairiness", "Scallywag"],
-    lnk: ["#", "#", "#"],
+    img: '/img/zone2m.png',
+    zone: 'zone2',
   },
   {
-    img: "/img/zone3m.png",
-    ent: ["Hairiness", "Aids", "curvy"],
-    lnk: ["#", "#", "#"],
+    img: '/img/zone3m.png',
+    zone: 'zone3',
   },
   {
-    img: "/img/zone4m.png",
-    ent: ["Shit Block", "Herpes", "Aids"],
-    lnk: ["#", "#", "#"],
+    img: '/img/zone4m.png',
+    zone: 'zone4',
   },
   {
-    img: "/img/zone5m.png",
-    ent: ["Shit Block", "Scallywag", "Herpes"],
-    lnk: ["#", "#", "#"],
+    img: '/img/zone5m.png',
+    zone: 'zone5',
   },
   {
-    img: "/img/zone6m.png",
-    ent: ["Hairiness", "curvy", "Shit Block"],
-    lnk: ["#", "#", "#"],
+    img: '/img/zone6m.png',
+    zone: 'zone6',
   },
   {
-    img: "/img/zone7m.png",
-    ent: ["a", "Herpes", "Hairiness"],
-    lnk: ["#", "#", "#"],
+    img: '/img/zone7m.png',
+    zone: 'zone7',
   },
-  { img: "/img/zone8m.png", ent: ["a", "b", "b"], lnk: ["#", "#", "#"] },
   {
-    img: "/img/zone9m.png",
-    ent: ["Scallywag", "curvy", "Aids"],
-    lnk: ["#", "#", "#"],
+    img: '/img/zone8m.png',
+    zone: 'zone8',
+  },
+  {
+    img: '/img/zone9m.png',
+    zone: 'zone9',
   },
 ];
 
 let idx = 0;
 
 function changeImage() {
-  if (idx > testArray.length - 1) {
+  if (idx > controlArray.length - 1) {
     idx = 0;
   } else if (idx < 0) {
-    idx = testArray.length - 1;
+    idx = controlArray.length - 1;
   }
-  img.src = testArray[idx].img;
-  updateList(testArray[idx].ent, testArray[idx].lnk);
+  img.src = controlArray[idx].img;
+  updateList(controlArray[idx].zone);
 }
 
-function updateList(ent, lnk) {
-  list.innerHTML = "";
-  for (i = 0; i < ent.length; i++) {
-    list.innerHTML += `<li><a href="${lnk[i]}">${ent[i]}<a/></li>`;
+function updateList(zone) {
+  let currentSet;
+  list.innerHTML = '';
+  currentSet = newDataSet.filter(function (item) {
+    return item.ACF.body_area.includes(zone);
+  });
+
+  for (i = 0; i < currentSet.length; i++) {
+    list.innerHTML += `<li><a href="${currentSet[i].slug}">${currentSet[i].title.rendered}<a/></li>`;
   }
 }
 
-rightBtn.addEventListener("click", () => {
+rightBtn.addEventListener('click', () => {
   idx++;
   changeImage();
 });
 
-leftBtn.addEventListener("click", () => {
+leftBtn.addEventListener('click', () => {
   idx--;
   changeImage();
 });
